d5c3e854ae8e79240a776f143d19e4e3feea53ba
Ivan Aguilar <ivanaguilar@spotahome.com>
2018-10-22 08:52:02 +0200

    Style application header

--- frontend/components/Meta.js
+++ frontend/components/Meta.js
@@ -2,10 +2,10 @@ import Head from 'next/head';
 
 const Meta = () => (
   <Head>
-    <meta name="viewport" content="width=device-width" initial-scale="1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
-    <link ref="shortcut icon" href="/static/favicon.png" />
-    <link ref="stylesheet" type="text/css" href="/static/nprogress.css" />
+    <link rel="shortcut icon" href="/static/favicon.png" />
+    <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
     <title>Sick Fits!</title>
   </Head>
 );
