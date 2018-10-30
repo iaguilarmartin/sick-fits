5e03aff385f475376daf687d0e1db6ef711a40a8
Ivan Aguilar Martin <iaguilarmartin@outlook.com>
2018-10-30 08:56:54 +0100

    Setup Prisma + Yoga

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,22 @@
-const mutations = {};
+const Mutations = {
+    async createItem(parent, args, ctx, info) {
+        // TODO: Check if they are logged in
 
-module.exports = mutations;
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+
+        return item;
+    }
+//     createDog(parent, args, ctx, info) {
+//         global.dogs = global.dogs || [];
+
+//         const newDog = { name: args.name };
+//         global.dogs.push(newDog);
+//         return newDog;
+//     }
+};
+
+module.exports = Mutations;
